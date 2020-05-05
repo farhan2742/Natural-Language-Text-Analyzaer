3d8be69a0c616056a8778193418746a0d1fd5cef
Farhan Kiyani <farhan2742@gmail.com>
2020-05-05 09:58:22 +0500

    Updated server
    comment out the development server start code so only heroku server start code remains

--- src/server/index.js
+++ src/server/index.js
@@ -139,9 +139,14 @@ function sendInfo(req, res) {
 }
 
 // designates what port the app will listen to for incoming requests
-app.listen(process.env.PORT, process.env.IP);
-/*
+
+/*DEV
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 });
+
 */
+//PROD
+
+app.listen(process.env.PORT, process.env.IP);
